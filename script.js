bcfbd3d8d62554a44de329a2d119b3f748ca7b17
sammiee2003 <samvandenbosch2003@gmail.com>
2019-11-05 12:17:57 +0100

    tada

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ document.getElementById("plaatje1").style.display ="none"
 document.getElementById("plaatje2").style.display="block"
 
 }
+function verander2() {
+    //hide foto 1
+    document.getElementById("plaatje2").style.display ="none"
+    //show foto 2
+    document.getElementById("plaatje3").style.display="block"
+    
+    }
+    function verander3() {
+        //hide foto 1
+        document.getElementById("plaatje3").style.display ="none"
+        //show foto 2
+        document.getElementById("plaatje4").style.display="block"
+        
+        }
+        function verander4() {
+            //hide foto 1
+            document.getElementById("plaatje4").style.display ="none"
+            //show foto 2
+            document.getElementById("plaatje5").style.display="block"
+            
+            }
+            function verander5() {
+                //hide foto 1
+                document.getElementById("plaatje5").style.display ="none"
+                //show foto 2
+                document.getElementById("plaatje6").style.display="block"
+                
+                }
