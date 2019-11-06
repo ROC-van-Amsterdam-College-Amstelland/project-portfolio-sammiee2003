8300a440bc76e7522b3041ba5ac0b26218e076dc
sammiee2003 <samvandenbosch2003@gmail.com>
2019-11-06 10:28:48 +0100

    final

--- script.js
+++ script.js
@@ -45,3 +45,28 @@ function verander2() {
                     document.getElementById("like").style.backgroundColor="black"
                     
                     }
+                    
+
+
+function initArray() {
+this.length = initArray.arguments.length;
+for (var i = 0; i < this.length; i++)
+this[i+1] = initArray.arguments[i];
+}
+
+var dagArray = new initArray("zondag","maandag", "dinsdag","woensdag","donderdag","vrijdag","zaterdag");
+
+var maandArray = new initArray("januari","februari","maart","april","mei","juni","juli", "augustus","september","oktober","november","december");
+
+var nu = new Date();
+var dagtekst = dagArray[(nu.getDay()+1)];
+var dag = nu.getDate();
+var maandtekst = maandArray[(nu.getMonth()+1)];
+var jaar = nu.getYear();
+var jaar4 = ((jaar < 1900) ? (jaar + 1900) : (jaar));
+
+var datumweergave = "Het is vandaag " + dagtekst + " " + dag + " " + maandtekst + " " + jaar4;
+
+document.getElementById("vandaag").innerHTML = datumweergave;
+
+
